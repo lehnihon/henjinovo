385d19b3c3d305d4a722852455d685e7692f2121
Leandro <lehnihon@hotmai.com>
2015-09-03 13:58:52 -0300

    parceiros coloridos

--- index.php
+++ index.php
@@ -299,19 +299,19 @@ get_header(); ?>
 			</div>
 			<div class="row parceiros">
 				<div class="col-md-2 col-md-offset-1">
-					<a target="_blank" href="http://www.abla.com.br"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro1.jpg"; ?>"/></a>		
+					<a target="_blank" href="http://www.abla.com.br"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro1b.jpg"; ?>"/></a>		
 				</div>	
 				<div class="col-md-2">
-					<a target="_blank" href="http://www.clickseminovos.com.br"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro2.jpg"; ?>"/></a>		
+					<a target="_blank" href="http://www.clickseminovos.com.br"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro2b.jpg"; ?>"/></a>		
 				</div>	
 				<div class="col-md-2">
-					<a target="_blank" href="http://www.yacp.com.br"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro3.jpg"; ?>"/></a>	
+					<a target="_blank" href="http://www.yacp.com.br"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro3b.jpg"; ?>"/></a>	
 				</div>	
 				<div class="col-md-2">
-					<a target="_blank" href="http://www.weblocadoras.com.br/portal/view/index.php"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro4.jpg"; ?>"/></a>		
+					<a target="_blank" href="http://www.weblocadoras.com.br/portal/view/index.php"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro4b.jpg"; ?>"/></a>		
 				</div>	
 				<div class="col-md-2">
-					<a target="_blank" href="http://www.segurototal.com.br/view/index.php"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro5.jpg"; ?>"/></a>	
+					<a target="_blank" href="http://www.segurototal.com.br/view/index.php"><img src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/parceiro5b.jpg"; ?>"/></a>	
 				</div>	
 			</div><br><br>
 		</div>
