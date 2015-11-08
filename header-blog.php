1d9cb170e3db2993e928b886b80bc05f530764b8
Leandro <lehnihon@hotmail.com>
2015-11-08 19:57:38 -0200

    correção js

--- header-blog.php
+++ header-blog.php
@@ -14,6 +14,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
