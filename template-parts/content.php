d08cbdf0ecd055f89fb668e4651ac438ed958b2a
Leandro <lehnihon@hotmail.com>
2015-11-10 20:58:17 -0200

    responsivo

--- template-parts/content.php
+++ template-parts/content.php
@@ -3,7 +3,7 @@
 		<?php if ( has_post_thumbnail() && !is_search() ) { ?>
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>">
 				<?php the_post_thumbnail('home-thumb', array(
-					'class' => "e-cinza",
+					'class' => "e-cinza img-responsive",
 				)); ?>
 			</a>
 		<?php } ?>
